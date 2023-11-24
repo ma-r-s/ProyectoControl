deab10e896333829fd73a08aecbac498548354c4
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2023-11-24 15:40:10 -0500

    Funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,18 @@
 #define SERV 16
 #define BOTON 32
 
-double Kp = 0.8;
-double Ki = 0.05;
-double Kd = 0;
+// Codigo magico
+//  double Kp = 0.597;
+//  double Ki = 0.931;
+//  double Kd = 0.0;
+
+double Kp = 0.7189;
+double Ki = 0.599;
+double Kd = 0.0;
 
 double input;
 double driverOut = 0;
-double setPoint = 50.0; // Change to float
+double setPoint = 30.0; // Change to float
 
 PID myPID(&input, &driverOut, &setPoint, Kp, Ki, Kd, DIRECT);
 
@@ -43,20 +48,25 @@ void control(void *pvParameters)
     double error = setPoint - input;
     myPID.Compute();
     double controlSignal = driverOut;
-    Serial.print("Input: ");
-    Serial.println(input);
-    Serial.print("Error: ");
-    Serial.println(error);
+    // Serial.print("Input: ");
+    // Serial.println(input);
+    // Serial.print("Error: ");
+    // Serial.println(error);
 
-    // Print the control signal value
-    Serial.print("Control Signal: ");
+    // // Print the control signal value
+    // Serial.print("Control Signal: ");
+    // Serial.println(controlSignal);
+    Serial.print(millis());
+    Serial.print(",");
     Serial.println(controlSignal);
 
     fly.write(driverOut);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
 
-float leerDistancia(int trigPin, int echoPin) // Change return type to float
+float prevDist = 0.0;
+float leerDistancia(int trigPin, int echoPin)
 {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -65,6 +75,21 @@ float leerDistancia(int trigPin, int echoPin) // Change return type to float
   digitalWrite(trigPin, LOW);
   long duration = pulseIn(echoPin, HIGH);
   float distance = duration * 0.034 / 2.0; // Change to float
+
+  // Set a threshold for acceptable difference
+  float threshold = 30.0;
+
+  // Check if the difference is too large
+  if (abs(distance - prevDist) > threshold)
+  {
+    // If so, return the previous distance
+    return prevDist;
+  }
+
+  // Update prevDist with the current distance
+  prevDist = distance;
+
+  // Return the current distance with an offset
   return distance - 7.77;
 }
 
